549fd9fd3590a7af92e00d732e4434ebc1bef168
ekaterinadubova <ekaterinadubova8@gmail.com>
2024-03-14 01:23:05 +0700

    commit

---
+++ administrator.py
@@ -0,0 +1,9 @@
+class administrator:
+
+    def __init__(self, FIO, phonenumber):
+        self.FIO = FIO
+        self.phonenumber = phonenumber
+
+a1 = administrator(FIO="Дубова Екатерина Анатольевна", phonenumber="89505981308")
+a2 = administrator(FIO="Забелина Анастасия Игоревна", phonenumber="89005552211")
+a3 = administrator(FIO="Полковникова Елизавета Сергеевна", phonenumber="89003331122")
