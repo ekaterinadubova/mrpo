d3c3d2d93744cbd01e207ad5ee02919b396dca25
ekaterinadubova <161693153+ekaterinadubova@users.noreply.github.com>
2024-03-14 01:38:22 +0700

    Add files via upload

---
+++ date.py
@@ -0,0 +1,14 @@
+import pet
+
+class date:
+
+    def __init__(self, datee, pet):
+        self.datee = datee
+        self.pet = pet
+
+d1 = date(datee="21.02.2024", pet=[p1])
+d2 = date(datee="18.01.2024", pet=[p2])
+d3 = date(datee="15.31.2023", pet=[p3])
+d4 = date(datee="03.02.2024", pet=[p4])
+d5 = date(datee="29.01.2024", pet=[p5])
+
