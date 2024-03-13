cd39ff4c3dc1c748c7a287670abdb86f037ff905
ekaterinadubova <161693153+ekaterinadubova@users.noreply.github.com>
2024-03-14 01:38:53 +0700

    Add files via upload

---
+++ aviary.py
@@ -0,0 +1,12 @@
+import pet
+
+class aviary:
+
+    def __init__(self, size, pet):
+        self.size = size
+        self.pet = pet
+
+
+av1 = aviary(size="Маленький", pet=[p1, p3])
+av2 = aviary(size="Большой", pet=[p2, p4, p5])
+
---
+++ kindOfAnimal.py
@@ -0,0 +1,9 @@
+
+class kindOfAnimal:
+
+    def __init__(self, kind):
+        self.kind = kind
+
+
+k1 = kindOfAnimal(kind="Кот")
+k2 = kindOfAnimal(kind="Собака")
