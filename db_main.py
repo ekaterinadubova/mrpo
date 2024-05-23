bd07865efced3135ee5bc63ccac76bbdf117593c
ekaterinadubova <161693153+ekaterinadubova@users.noreply.github.com>
2024-05-23 12:29:33 +0700

    Update db_main.py

--- db_main.py
+++ db_main.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from db_setup import Base, Food, User, Aviary, Pet
+from db_setup import Base, User, Aviary, Pet
 
 DATABASE_URL = "sqlite:///pet.db"
 
@@ -9,15 +9,15 @@ Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# Создаие экземпляров волонтёров
+# Создание экземпляров волонтёров
 user1 = User(name="Zolotoeva Anna", phone="123456789")
 user2 = User(name="Ramid Alina", phone="987654321")
 
-# Создаие экземпляров животных
+# Создание экземпляров животных
 pet1 = Pet(kind="Cat", name="Cused", breed="Britain", gender="Male", size="Small")
 pet2 = Pet(kind="Dog", name="Baloon", breed="Shepherd", gender="Female", size="Big")
 
-# Создаие экземпляров вольеров
+# Создание экземпляров вольеров
 aviary1 = Aviary(size="Big")
 aviary2 = Aviary(size="Small")
 
@@ -25,22 +25,14 @@ aviary2 = Aviary(size="Small")
 aviary1.pets.append(pet2)
 aviary2.pets.append(pet1)
 
-# Создание экземпляров приюта
-food = Food(name="Royal Canin", users=[user1], aviaries=[aviary2], pets=[pet1])
-
-# Добавление данных в бд
-session.add(food)
+# Добавление данных в базу данных
+session.add(user1)
+session.add(user2)
+session.add(pet1)
+session.add(pet2)
+session.add(aviary1)
+session.add(aviary2)
 session.commit()
 
-# Запрос в бд на чтение
-food = session.query(Food).all()
-for food in Food:
-    print(food)
-    for user in food.users:
-        print(f"  {user}")
-    for aviary in food.aviaries:
-        print(f"  {aviary}")
-        for pet in aviary.pets:
-            print(f"    {pet}")
 
 session.close()
